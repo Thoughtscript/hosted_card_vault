6cb4842fa8e1e90b95e8f2ed31847cf0186faefb
Adam Gerard <adam.intae.gerard@protonmail.com>
2025-05-26 13:16:11 -0500

    fix: use getAttribute since browser safe, add max retries

--- script.js
+++ script.js
@@ -20,6 +20,30 @@ window.onload = function () {
         })
     }
 
+    const retryImage = (e) => {
+        const EL = e.currentTarget,
+            NM = EL.alt || EL.getAttribute("data-attr"),
+            MR = parseInt(EL.getAttribute("max-retries")),
+            CR = parseInt(EL.getAttribute('current-retries'))
+
+        if (CR < MR) {
+            console.log(`${ NM } image loading failed - retrying ${CR + 1} / ${MR}`)
+
+            let O = EL.src
+            if (O.length) {
+                EL.src = ""
+                EL.src = O
+            }
+
+            EL.setAttribute("current-retries", CR + 1)
+        }
+
+        if (CR >= MR) {
+            console.log(`${ NM } max retries ${MR} reached - removing Event Listener...`)
+            EL.removeEventListener('error', retryImage)
+        }
+    }
+
     // Failed to load resource: net::ERR_HTTP2_PROTOCOL_ERROR Large Images loaded but not cached from GitHub (no CDN).
     const setErrorEventListeners = () => {
         const ELS = document.getElementsByClassName('image')
@@ -27,19 +51,9 @@ window.onload = function () {
         // TBD - exponential backoff...
         // Probably unnecessary here for GitHub hosting but would be of use elsewhere.
         for (let i = 0; i < ELS.length; i++) {
-            ELS[i].addEventListener('error', (e) => {
-                const EL = e.currentTarget,
-                    C = EL.alt || EL.attributes[2].textContent
-                
-                console.log(`${ C } image loading failed - retrying...`)
-
-                let O = EL.src
-
-                if (O.length) {
-                    EL.src = ""
-                    EL.src = O
-                }
-            })
+            ELS[i].setAttribute('max-retries', 3)
+            ELS[i].setAttribute('current-retries', 0)
+            ELS[i].addEventListener('error', retryImage)
         }
     }
     
